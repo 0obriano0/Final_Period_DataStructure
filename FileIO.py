751566ccc84bd8f6789f2e6d33251ff80b241abd
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2019-05-26 22:28:18 +0800

    create FileIO

---
+++ FileIO.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 26 15:27:48 2019
+
+@author: brian
+"""
+import os
+import database
+import pandas as pd
+    
+#=============================== function =================================
+def loadFile(url):
+    if os.path.isfile(url):
+        df = pd.read_csv(url)
+    else:
+        if "vendor" in url:
+            NewList = [["","","","","",""]]
+            columns_list = ["name","number","principle","address","RN","product"]
+        else:
+            NewList = ["","","","","","",""]
+            columns_list = ["number","name","SN","warranty","volume","weight","category"]
+        df = pd.DataFrame(NewList, columns = columns_list)
+        df.to_csv(url,encoding='utf_8_sig' , index = False)
+    return df
+
+def loadvendor():
+    loadFile("./vendor.csv")
+
+def loadproduct():
+    loadFile("./product.csv")
+
+def updata(data_tpye,data_database):
+    
+
+def upload
+    
+#=============================== main =================================
+if __name__ == '__main__':
+    #=============================== test =================================
+    list_data = []
+    
+    for index in range(5):
+        a = database.vendor("brian",123456)
+        list_data.append(a)
+        
+    list_data[0].name = "a"
+    list_data[0].RN = 123456789654
+        
+    
+    print(list_data[0].name)
+    
+    print(list_data[1].name)
+    loadvendor()
