303dd14b184c613aa5eefa0c6fd3f683f2a3c313
andy <ab123246@gmail.com>
2019-05-28 21:54:21 +0800

    creat_UI

---
+++ UI.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 20:49:06 2019
+
+@author: asus
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statistics
+
