7fa67540c069888c7698ff71a6f1b2c673cf1a7d
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2019-06-01 16:27:30 +0800

    增加 輸出檔案 的函式

--- FileIO.py
+++ FileIO.py
@@ -6,9 +6,8 @@ Created on Sun May 26 15:27:48 2019
 """
 import database
 import yaml
-    
 #=============================== function =================================
-def LoadYaml(url):
+def loadYaml(url):
     try:
         with open(url, "r", encoding="utf-8") as stream:
             data = yaml.load(stream)
@@ -16,11 +15,11 @@ def LoadYaml(url):
         data = None
     return data
 
-def LoadData(name,typeSelect):
+def loadData(name,typeSelect):
     typeCheck = ["vendor","product"]
     if typeSelect not in typeCheck:
         return None
-    load_data = LoadYaml("./" + typeSelect + "/" + name)
+    load_data = loadYaml("./" + typeSelect + "/" + name)
     if load_data is not None:
         if typeSelect in load_data:
             return load_data[typeSelect]
@@ -28,6 +27,46 @@ def LoadData(name,typeSelect):
     else:
         print("查無此檔案")
     return None
+
+def createYaml(data_database,typeSelect):
+    final_dict = {}
+    url = ""
+    if data_database is not None:
+        secand_dict = {}
+        if typeSelect  == "vendor":
+            url = "./" + typeSelect + "/" + data_database.name + ".yml"
+            if loadYaml(url) is not None:
+                print("此檔案已經存在")
+                return 0
+            secand_dict["name"] = data_database.name
+            secand_dict["number"] = data_database.number
+            secand_dict["RN"] = data_database.RN
+            secand_dict["principle"] = data_database.principle
+            secand_dict["address"] = data_database.address
+            secand_dict["product"] = data_database.product
+        elif typeSelect == "product":
+            url ="./" + typeSelect + "/" + data_database.number + ".yml"
+            if loadYaml(url) is not None:
+                print("此檔案已經存在")
+                return 0
+            secand_dict["name"] = data_database.name
+            secand_dict["number"] = data_database.number
+            secand_dict["SN"] = data_database.SN
+            secand_dict["warranty"] = data_database.warranty
+            secand_dict["volume"] = data_database.volume
+            secand_dict["weight"] = data_database.weight
+            secand_dict["category"] = data_database.category
+        else:
+            print("傳進來的資料結構有問題")
+            return 0
+        final_dict[typeSelect] = secand_dict
+        #with open(url, 'w', encoding="utf-8") as outfile:
+        with open(url, 'w', encoding="utf-8") as outfile:
+            yaml.dump(final_dict, outfile, default_flow_style=False, encoding=('utf-8'), allow_unicode=True)
+        return 1
+    else:
+        print("傳入的資料結構是: None")
+        return 0
 
 def formatVendor(data_dict):
     return database.vendor(data_dict["name"],data_dict["RN"],data_dict)
@@ -41,6 +80,11 @@ def formatVendor_list(data_list):
 #=============================== main =================================
 if __name__ == '__main__':
     #=============================== test =================================
-    data_dict = LoadData("聲寶.yml","vendor")
-    a = formatVendor(data_dict)
-    print(a.principle)
+    a = []
+    data_dict = loadData("聲寶.yml","vendor")
+    a.append(formatVendor(data_dict))
+    print(a[0].principle)
+    a[0].name = "abc"
+    createYaml(a[0],"vendor")
+    data_dict = loadData("abc.yml","vendor")
+    a.append(formatVendor(data_dict))
