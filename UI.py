85f9987b297292bf39b9e0cad533b4c447f43fdb
andy <ab123246@gmail.com>
2019-06-07 16:00:30 +0800

    select_
    select demo

--- UI.py
+++ UI.py
@@ -8,6 +8,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import statistics
+import search as sb
+
 
 def getinfo(inputstring):
     listwords = inputstring.split(" ")
@@ -15,25 +17,29 @@ def getinfo(inputstring):
     if(listwords[0]=='select' and listwords[2]=='from' and listwords[4] == 'where'):
         dic = {}
         dicwhere = {} 
-        print('selecting')
         select_ = listwords[1].replace('"','').split(',')
         from_ = listwords[3].replace('"','')
         where_ = listwords[5].replace('"','').split(',')
-        print(where_)
         for i in where_:
             buffer = i.split('=')
             dicwhere[buffer[0]] = buffer[1]
-            print(dicwhere)
+        dic['select'] = select_
+        dic['from'] = from_
+        dic['where'] = dicwhere
+        print(dic)
+        return dic
     else:
         print("語法錯誤")
+        return(-999999999)
     
 #==================================================#
 #input
 #==================================================#
-fakesrting1 = 'select "name","RN" from "vender" where "name"="UX501VW","rn"="4654654"' #output 'UX501VW' '23638777'
-fakestring2 = 'select * from "vendor" where "name" = "ASUS"' #output 'UX501VW' 'ASUS' '23638777' '施先生' '台北市承德路' '26073'
-fakestring3 = 'select * from "vendor" where "RN" = "23638777"' #output 'UX501VW' 'ASUS' '23638777' '施先生' '台北市承德路' '26073'
-
-
-a = {"select":["name","RN"],"from":"vendor","where":{"name":"UX501VM"}}
-a = getinfo(fakesrting1)
+if __name__ == '__main__' :
+    fakesrting1 = 'select "name","RN" from "vender" where "name"="UX501VW","rn"="4654654"' #output 'UX501VW' '23638777'
+    fakestring2 = 'select * from "vendor" where "name"="ASUS"' #output 'UX501VW' 'ASUS' '23638777' '施先生' '台北市承德路' '26073'
+    fakestring3 = 'select "name","address" from "vendor" where "RN"="aasswwwddd555"' #output 'UX501VW' 'ASUS' '23638777' '施先生' '台北市承德路' '26073'
+    
+    search_ = sb.search()
+    a = getinfo(fakestring3)
+    info = search_.require(a)
