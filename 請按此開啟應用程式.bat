7c094386d1d9e22a6e2f3ac7aa55a35cc3700925
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2019-06-13 21:04:04 +0800

    增加免安裝python

---
+++ 請按此開啟應用程式.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET BINDIR=%~dp0
+CD /D "%BINDIR%"
+:start
+cls
+".\Portable python-3.7.3 x64\App\Python\"python UI.py
+@echo 程式已結束 現在可按視窗右上角的 X 關閉程式 否則將於10秒後自動重新啟動
+@ping 127.0.0.1 -n 10 -w 100 > nul
+goto start
